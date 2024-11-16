4640b1d7ee9cb32ffabdef6c8f7698e6516caa33
Eduardo Fransico Lugo Quintana <info@camaleon-studio.com>
2024-11-16 17:14:49 +0100

    Acomodo de Trabajos

--- src/Components/Trabajo.jsx
+++ src/Components/Trabajo.jsx
@@ -6,12 +6,12 @@ const Trabajo = () => {
         <section className='trabajo' id="trabajo">
             <h2>NUESTRO TRABAJO</h2>
             <article className="trabajo-cards-container">
+                <InstagramEmbed url="https://www.instagram.com/politica_salvaje/" width={550} />
                 <InstagramEmbed url="https://www.instagram.com/akampa.mx/" width={550} />
-                <InstagramEmbed url="https://www.instagram.com/watches_world_mag/" width={550} />
             </article>
             <article className="trabajo-cards-container">
+                <InstagramEmbed url="https://www.instagram.com/watches_world_mag/" width={550} />
                 <InstagramEmbed url="https://www.instagram.com/muro.proyectos/" width={550} />
-                <InstagramEmbed url="https://www.instagram.com/politica_salvaje/" width={550} />
             </article>
         </section>
         
